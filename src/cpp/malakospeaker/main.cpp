1e6cb5953b1d7f397e40c94f833d23f005a2615f
Rodrigo Magalhaes <rmagalhaes85@gmail.com>
2011-10-07 20:07:13 -0300

    Arquivos diversos

--- src/cpp/malakospeaker/main.cpp
+++ src/cpp/malakospeaker/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+#include <cstdio>
 #include <windows.h>
 #include <shellapi.h>
+#include <strsafe.h>
 
 int main(void) {
 	std::cout << "Servico de alertas sonoros do MalakoMine" << std::endl;
-	NOTIFYICONDATA nid = {};
-	// nid.cbSize = 
 	
+	HICON icon;
+	
+	NOTIFYICONDATA nid = {};
+	nid.cbSize = sizeof(nid);
+	// nid.hWnd = hWnd;
+	nid.uFlags = NIF_ICON | NIF_TIP | NIF_GUID;
+	nid.uID = 0;
+	// StringCchCopy(nid.szTip, 40, L"Malako Sound Service");
+	&nid.szTip = "Malako Sound Service";
+	Shell_NotifyIcon(NIM_ADD, &nid);
+	
+	std::cout << "Pressione qualquer tecla para encerrar..." << std::endl;
+	char c;
+	std::cin >> c;
 	
 	return 0;
 }
