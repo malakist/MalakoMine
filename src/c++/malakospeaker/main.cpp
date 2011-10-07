60ae4c1eed129084a219e0ffecdcf631b6d5c2b1
Rodrigo Magalhaes <rmagalhaes85@gmail.com>
2011-10-07 13:49:19 -0300

    Testes para area de notificacoes

--- src/c++/malakospeaker/main.cpp
+++ src/c++/malakospeaker/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <windows.h>
+#include <shellapi.h>
 
 int main(void) {
 	std::cout << "Servico de alertas sonoros do MalakoMine" << std::endl;
+	NOTIFYICONDATA nid = {};
+	// nid.cbSize = 
+	
+	
 	return 0;
 }
