0873972a33c3c459df2b7d16fbf92e074b3c0318
Rodrigo <rmagalhaes85@gmail.com>
2011-10-07 23:24:50 -0300

    Producao do windows service em .net

---
+++ src/MalakoService/Malako.TFSProxy/Proxy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Malako.TFSProxy
+{
+    public class Proxy : IDisposable
+    {
+        public void Inicializar()
+        {
+
+        }
+
+        public List<Task> GetTasks()
+        {
+
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ src/MalakoService/Malako.TFSProxy/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Malako.TFSProxy
+{
+    class Task
+    {
+        public int Id { get; set; }
+        public DateTime DataDigitacao { get; set; }
+    }
+}
